e39ba7f8dd85f82367c4d354dcdb5fa6635a08b6
Kern Herskind Nightingale <khe@sitecore.net>
2013-08-21 10:52:33 +0100

    Added MVC support to fields (now implements IHtmlString)

--- Fortis/Model/IFieldWrapper.cs
+++ Fortis/Model/IFieldWrapper.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace Fortis.Model
 {
-	public interface IFieldWrapper : IWrapper
+    public interface IFieldWrapper : IWrapper, IHtmlString
 	{
 		string RawValue { get; set; }
 		bool Modified { get; }
