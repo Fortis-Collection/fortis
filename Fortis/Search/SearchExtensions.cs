19135d02e254d480f417f703f18a5195fd419b46
Jason Bert <jason.bert@lightmaker.com>
2013-12-17 22:46:07 +0000

    Changed ids parameter to values and made it so the type matches the TKey from the expression

--- Fortis/Search/SearchExtensions.cs
+++ Fortis/Search/SearchExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,27 +8,28 @@ namespace Fortis.Search
 {
 	public static class SearchExtensions
 	{
-		public static IQueryable<TSource> ContainsIdOr<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable<Guid> ids)
+		public static IQueryable<TSource> ContainsOr<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values) where TKey : IEnumerable
 		{
-			return ContainsId(queryable, keySelector, ids, true);
+			return Contains(queryable, keySelector, values, true);
 		}
 
-		public static IQueryable<TSource> ContainsIdAnd<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable<Guid> ids)
+		public static IQueryable<TSource> ContainsAnd<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values) where TKey : IEnumerable
 		{
-			return ContainsId(queryable, keySelector, ids, false);
+			return Contains(queryable, keySelector, values, false);
 		}
 
-		private static IQueryable<TSource> ContainsId<TSource, TKey>(IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable<Guid> ids, bool or)
+		private static IQueryable<TSource> Contains<TSource, TKey>(IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values, bool or) where TKey : IEnumerable
 		{
 			if (!(keySelector.Body is MemberExpression))
 			{
 				throw new InvalidOperationException("Fortis: Expression must be a member expression");
 			}
 
+			var castedValues = values.Cast<object>();
 			var returnQueryable = queryable;
 			var typeofT = typeof(TSource);
 			var parameter = Expression.Parameter(typeofT);
-			var constants = ids.Select(id => Expression.Constant(id));
+			var constants = castedValues.Select(id => Expression.Constant(id));
 			var expressions = constants.Select(constant => Expression.Call(typeof(Enumerable), "Contains", new Type[] { typeof(Guid) }, keySelector.Body, constant));
 
 			var aggregateExpressiosn = expressions.Select(expression => (Expression)expression).Aggregate((x, y) => or ? Expression.OrElse(x, y) : Expression.AndAlso(x, y));
