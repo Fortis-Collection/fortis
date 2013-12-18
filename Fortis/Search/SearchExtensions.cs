d3236efdf94b75b0dc531362bd522d5df439b7fd
Jason Bert <jason.bert@lightmaker.com>
2013-12-18 12:40:08 +0000

    Changed implementation to try and find a matching contains method on the TKey type first (e.g. string) and use that instead of Enumerable

--- Fortis/Search/SearchExtensions.cs
+++ Fortis/Search/SearchExtensions.cs
@@ -8,29 +8,31 @@ namespace Fortis.Search
 {
 	public static class SearchExtensions
 	{
-		public static IQueryable<TSource> ContainsOr<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values) where TKey : IEnumerable
+		public static IQueryable<TSource> ContainsOr<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable values) where TKey : IEnumerable
 		{
 			return Contains(queryable, keySelector, values, true);
 		}
 
-		public static IQueryable<TSource> ContainsAnd<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values) where TKey : IEnumerable
+		public static IQueryable<TSource> ContainsAnd<TSource, TKey>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable values) where TKey : IEnumerable
 		{
 			return Contains(queryable, keySelector, values, false);
 		}
 
-		private static IQueryable<TSource> Contains<TSource, TKey>(IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values, bool or) where TKey : IEnumerable
+		private static IQueryable<TSource> Contains<TSource, TKey>(IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, IEnumerable values, bool or) where TKey : IEnumerable
 		{
+			const string methodName = "Contains";
+
 			// Ensure the body of the selector is a MemberExpression
 			if (!(keySelector.Body is MemberExpression))
 			{
 				throw new InvalidOperationException("Fortis: Expression must be a member expression");
 			}
 
-			var typeofTSource = typeof(TSource);
-			var typeofTKey = typeof(TKey);
+			var typeOfTSource = typeof(TSource);
+			var typeOfTKey = typeof(TKey);
 
 			// x
-			var parameter = Expression.Parameter(typeofTSource);
+			var parameter = Expression.Parameter(typeOfTSource);
 
 			// Create the enumerable of constant expressions based off of the values
 			var constants = values.Cast<object>().Select(id => Expression.Constant(id));
@@ -51,7 +53,18 @@ namespace Fortis.Search
 			//
 			// Each expression will effectively be like running the following;
 			// x => x.MyIdListField.Contains(AnId)
-			var expressions = constants.Select(constant => Expression.Call(typeof(Enumerable), "Contains", typeofTKey.GetGenericArguments(), keySelector.Body, constant));
+			IEnumerable<MethodCallExpression> expressions = Enumerable.Empty<MethodCallExpression>();
+
+			if (typeOfTKey.GetMethods().Any(m => m.Name.Equals(methodName)))
+			{
+				var method = typeOfTKey.GenericTypeArguments.Any() ? typeOfTKey.GetMethod(methodName, typeOfTKey.GenericTypeArguments) : typeOfTKey.GetMethod(methodName);
+
+				expressions = constants.Select(constant => Expression.Call(keySelector.Body, method, constant));
+			}
+			else
+			{
+				expressions = constants.Select(constant => Expression.Call(typeof(Enumerable), methodName, typeOfTKey.GenericTypeArguments, keySelector.Body, constant));
+			}
 
 			// Combine all the expressions into one expression so you would end with something like;
 			// x => x.MyIdListField.Contains(AnId) OR x.MyIdListField.Contains(AnId) OR x.MyIdListField.Contains(AnId)
