cccf23a6512b90ef2907390c86794189c4d0553a
Jason Bert <jason.bert@lightmaker.com>
2013-12-18 00:08:51 +0000

    Added commenting in and fixed a bug with argument types

--- Fortis/Search/SearchExtensions.cs
+++ Fortis/Search/SearchExtensions.cs
@@ -20,22 +20,47 @@ namespace Fortis.Search
 
 		private static IQueryable<TSource> Contains<TSource, TKey>(IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, TKey values, bool or) where TKey : IEnumerable
 		{
+			// Ensure the body of the selector is a MemberExpression
 			if (!(keySelector.Body is MemberExpression))
 			{
 				throw new InvalidOperationException("Fortis: Expression must be a member expression");
 			}
 
-			var castedValues = values.Cast<object>();
-			var returnQueryable = queryable;
-			var typeofT = typeof(TSource);
-			var parameter = Expression.Parameter(typeofT);
-			var constants = castedValues.Select(id => Expression.Constant(id));
-			var expressions = constants.Select(constant => Expression.Call(typeof(Enumerable), "Contains", new Type[] { typeof(Guid) }, keySelector.Body, constant));
+			var typeofTSource = typeof(TSource);
+			var typeofTKey = typeof(TKey);
 
-			var aggregateExpressiosn = expressions.Select(expression => (Expression)expression).Aggregate((x, y) => or ? Expression.OrElse(x, y) : Expression.AndAlso(x, y));
-			var lambda = Expression.Lambda<Func<TSource, bool>>(aggregateExpressiosn, parameter);
+			// x
+			var parameter = Expression.Parameter(typeofTSource);
 
-			return returnQueryable.Where(lambda);
+			// Create the enumerable of constant expressions based off of the values
+			var constants = values.Cast<object>().Select(id => Expression.Constant(id));
+
+			// Create separate MethodCallExpression objects for each constant expression created
+			// type -> we need to specify the type which contains the method we want to run
+			// methodName -> in this instance we need to specify the Contains method
+			// typeArguments -> the type parameter from TKey
+			//					e.g. if we're passing through IEnumerable<Guid> then this will pass through the Guid type
+			//					this is because we're effectively running IEnumerable<Guid>.Contains(Guid guid) for each
+			//					guid in our values object
+			// arguments ->
+			//		keySelector.Body -> this would be property we want to run the expession on e.g.
+			//							IQueryable<MyPocoTemplate>.Where(x => x.MyIdListField)
+			//							so keySelector.Body will contain the "x.MyIdListField" which is what we want to run
+			//							each constant expression against
+			//		constant -> this is the constant expression
+			//
+			// Each expression will effectively be like running the following;
+			// x => x.MyIdListField.Contains(AnId)
+			var expressions = constants.Select(constant => Expression.Call(typeof(Enumerable), "Contains", typeofTKey.GetGenericArguments(), keySelector.Body, constant));
+
+			// Combine all the expressions into one expression so you would end with something like;
+			// x => x.MyIdListField.Contains(AnId) OR x.MyIdListField.Contains(AnId) OR x.MyIdListField.Contains(AnId)
+			var aggregateExpressions = expressions.Select(expression => (Expression)expression).Aggregate((x, y) => or ? Expression.OrElse(x, y) : Expression.AndAlso(x, y));
+
+			// Create the Lambda expression which can be passed to the .Where
+			var lambda = Expression.Lambda<Func<TSource, bool>>(aggregateExpressions, parameter);
+
+			return queryable.Where(lambda);
 		}
 	}
 }
