6f2cdcab2ea230940ef9c535aa9972092b88a1b9
Jason Bert <jason.bert@lightmaker.com>
2014-01-12 11:34:56 +0000

    Implemented IntegerFieldWrapper for rendering parameters

---
+++ Fortis/Model/RenderingParameters/Fields/IntegerFieldWrapper.cs
@@ -0,0 +1,36 @@
+﻿using Fortis.Model.Fields;
+using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortis.Model.RenderingParameters.Fields
+{
+	public class IntegerFieldWrapper : FieldWrapper, IIntegerFieldWrapper
+	{
+		private long? _value;
+
+		public IntegerFieldWrapper(string value)
+			: base(value) { }
+
+		public long Value
+		{
+			get
+			{
+				if (!_value.HasValue)
+				{
+					long parsedValue;
+
+					if (long.TryParse(RawValue, out parsedValue))
+					{
+						_value = parsedValue;
+					}
+				}
+
+				return _value.Value;
+			}
+		}
+	}
+}
