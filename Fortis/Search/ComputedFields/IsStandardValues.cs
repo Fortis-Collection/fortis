fc980776d2a57ac2a2b90460a6d05a1395f33c7d
Jason Bert <jason.bert@goblinrockets.com>
2014-11-18 15:35:15 +0000

    Pluralised standard values computed index field

--- Fortis/Search/ComputedFields/IsStandardValues.cs
+++ Fortis/Search/ComputedFields/IsStandardValues.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Fortis.Search.ComputedFields
 {
-	public class IsStandardValue : AbstractComputedIndexField
+	public class IsStandardValues : AbstractComputedIndexField
 	{
 		public override object ComputeFieldValue(IIndexable indexable)
 		{
