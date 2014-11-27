011491750a6e32e90442b3a36b7bf86760833b3e
Jason Bert <jason.bert@goblinrockets.om>
2014-11-27 15:52:32 +0000

    Updated the initialisation process to include the item search factory

--- Fortis/Global.cs
+++ Fortis/Global.cs
@@ -1,5 +1,6 @@
 ﻿using Fortis.Model;
 using Fortis.Providers;
+using Fortis.Search;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Fortis
 	{
 		public static ISpawnProvider SpawnProvider;
 		public static IItemFactory ItemFactory;
+		public static IItemSearchFactory ItemSearchFactory;
 
 		/// <summary>
 		/// Initialise a default Fortis setup with a context provider and verifies the initialisation.
@@ -19,15 +21,12 @@ namespace Fortis
 		/// <param name="contextProvider"></param>
 		public static void Initialise(IContextProvider contextProvider)
 		{
-			SpawnProvider = new SpawnProvider(
-									new TemplateMapProvider(
-											new ModelAssemblyProvider()
-										)
-								);
+			var templateMapProvider = new TemplateMapProvider(new ModelAssemblyProvider());
+			var spawnProvider = new SpawnProvider(templateMapProvider);
+			var itemFactory = new ItemFactory(contextProvider, SpawnProvider);
+			var itemSearchFactory = new ItemSearchFactory(templateMapProvider, new SearchResultsAdapter());
 
-			ItemFactory = new ItemFactory(contextProvider, SpawnProvider);
-
-			Verify();
+			Initialise(spawnProvider, itemFactory, itemSearchFactory);
 		}
 
 		/// <summary>
@@ -35,18 +34,31 @@ namespace Fortis
 		/// </summary>
 		/// <param name="spawnProvider"></param>
 		/// <param name="itemFactory"></param>
-		public static void Initialise(ISpawnProvider spawnProvider, IItemFactory itemFactory)
+		public static void Initialise(ISpawnProvider spawnProvider, IItemFactory itemFactory, IItemSearchFactory itemSearchFactory)
 		{
 			SpawnProvider = spawnProvider;
 			ItemFactory = itemFactory;
+			ItemSearchFactory = itemSearchFactory;
 
-			Verify();
+			Verify(ItemFactory, SpawnProvider, ItemSearchFactory);
 		}
 
 		/// <summary>
 		/// Verifies that Fortis has been correctly set up.
 		/// </summary>
+		[Obsolete("Use method with parameters")]
 		public static void Verify()
+		{
+			Verify(ItemFactory, SpawnProvider, ItemSearchFactory);
+		}
+
+		/// <summary>
+		/// Verifies that Fortis has been correctly set up.
+		/// </summary>
+		/// <param name="itemFactory"></param>
+		/// <param name="spawnProvider"></param>
+		/// <param name="itemSearchFactory"></param>
+		public static void Verify(IItemFactory itemFactory, ISpawnProvider spawnProvider, IItemSearchFactory itemSearchFactory)
 		{
 			var verificationFailed = false;
 			var errorMessages = new StringBuilder();
@@ -54,15 +66,21 @@ namespace Fortis
 			errorMessages.AppendLine("Fortis intialise verification failed:");
 			errorMessages.AppendLine();
 
-			if (!VerifySpawnProvider())
+			if (!VerifySpawnProvider(spawnProvider))
 			{
 				errorMessages.AppendLine("SpawnProvider is not initialised");
 				verificationFailed = true;
 			}
 
-			if (!VerifyItemFactory())
+			if (!VerifyItemFactory(itemFactory))
 			{
-				errorMessages.AppendLine("ItemFacotry is not initialised");
+				errorMessages.AppendLine("ItemFactory is not initialised");
+				verificationFailed = true;
+			}
+
+			if (!VerifyItemSearchFactory(itemSearchFactory))
+			{
+				errorMessages.AppendLine("ItemSearchFactory is not initialised");
 				verificationFailed = true;
 			}
 
@@ -76,18 +94,49 @@ namespace Fortis
 		/// Verifies the item factory is correctly set up.
 		/// </summary>
 		/// <returns></returns>
+		[Obsolete("Use method with parameter")]
 		public static bool VerifyItemFactory()
 		{
-			return ItemFactory != null;
+			return VerifyItemFactory(ItemFactory);
+		}
+
+		/// <summary>
+		/// Verifies the item factory is correctly set up.
+		/// </summary>
+		/// <param name="itemFactory"></param>
+		/// <returns></returns>
+		public static bool VerifyItemFactory(IItemFactory itemFactory)
+		{
+			return itemFactory != null;
 		}
 
 		/// <summary>
 		/// Verifies the spawn provider is correctly set up.
 		/// </summary>
 		/// <returns></returns>
+		[Obsolete("Use method with parameter")]
 		public static bool VerifySpawnProvider()
 		{
-			return SpawnProvider != null;
+			return VerifySpawnProvider(SpawnProvider);
+		}
+
+		/// <summary>
+		/// Verifies the spawn provider is correctly set up.
+		/// </summary>
+		/// <param name="spawnProvider"></param>
+		/// <returns></returns>
+		public static bool VerifySpawnProvider(ISpawnProvider spawnProvider)
+		{
+			return spawnProvider != null && spawnProvider.TemplateMapProvider != null;
+		}
+
+		/// <summary>
+		/// Verifies the item search factory is correctly set up
+		/// </summary>
+		/// <returns></returns>
+		public static bool VerifyItemSearchFactory(IItemSearchFactory itemSearchFactory)
+		{
+			return itemSearchFactory != null;
 		}
 	}
 }
