06121b8bdc9f6d90f8984ad3706349d0b62fee97
Richard Seal <richard.seal@lightmaker.com>
2016-03-09 23:27:05 -0500

    Updated sample config and tests for the Model assemblies

--- src/Fortis.Tests/Configuration/XmlConfigurationProviderTests.cs
+++ src/Fortis.Tests/Configuration/XmlConfigurationProviderTests.cs
@@ -15,7 +15,7 @@
 			Assert.IsNotEmpty(testProvider.DefaultConfiguration.Models);
 			Assert.IsNotNull(testProvider.DefaultConfiguration.Models.FirstOrDefault(x => x.Name == "Fortis.Model"));
 			Assert.IsNotNull(testProvider.DefaultConfiguration.Models.FirstOrDefault(x => x.Name == "Project.Model"));
-			Assert.IsNotNull(testProvider.DefaultConfiguration.Models.FirstOrDefault(x => x.Assembley == "Fortis.Model, Fortis"));
+			Assert.IsNotNull(testProvider.DefaultConfiguration.Models.FirstOrDefault(x => x.Assembley == "Fortis.Model, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
 		}
 	}
 }
