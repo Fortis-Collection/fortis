435b990a5440b60ce2a1d78405948254c7bb6eed
Jason Bert <jason.bert@goblinrockets.com>
2016-10-25 20:11:41 +0100

    Added test for field attribute name parser

---
+++ src/Fortis.Test/Fields/PropertyInfoFieldNameParserTests.cs
@@ -0,0 +1,26 @@
+﻿using Fortis.Fields;
+using Xunit;
+
+namespace Fortis.Test.Fields
+{
+	public class PropertyInfoFieldNameParserTests
+	{
+		[Fact]
+		public void Parse_Attribute_SpecifiedAttributeName()
+		{
+			var propertyInfo = typeof(ITestModel).GetProperty("TestField");
+			var propertyInfoFieldNameParser = new PropertyInfoFieldNameParser(null);
+
+			var expected = "Test";
+			var actual = propertyInfoFieldNameParser.Parse(propertyInfo);
+
+			Assert.Equal(expected, actual);
+		}
+
+		public interface ITestModel
+		{
+			[Field("Test")]
+			string TestField { get; set; }
+		}
+	}
+}
